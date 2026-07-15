469ddaff42194c7ba519a6e79609a8e4023899d3
Haisham <mhaisham79@gmail.com>
2026-07-15 21:06:40 +0500

    Add pull command module with stub functions

---
+++ src/commands/pull.rs
@@ -0,0 +1,5 @@
+use crate::{context::AppContext, project::Project};
+
+pub async fn pull_all_projects(context: AppContext) {}
+
+async fn pull_project(context: &mut AppContext, project: &Project) {}
